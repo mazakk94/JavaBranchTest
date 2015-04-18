0a322eb925f2faae39bfefb0f3642937ea401faf
MaciejWojtasik <maciej_wojtasik@o2.pl>
2015-04-18 15:41:56 +0200

    added calculator to master's

---
+++ src/master/Calculator.java
@@ -0,0 +1,11 @@
+package master;
+
+public class Calculator {
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+    
+    public static float division(int a, int b){
+        return a / (float)b;
+    }
+}
