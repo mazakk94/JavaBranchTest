904ac5f56a759ce66380677c14ca5a864224da4c
MaciejWojtasik <maciej_wojtasik@o2.pl>
2015-04-19 00:54:58 +0200

    added class named Printing

---
+++ src/master/Printing.java
@@ -0,0 +1,9 @@
+package master;
+
+public class Printing {
+    /*this class will be printing messages in console log*/
+    public void PrintHello(){
+        System.out.println("Hello world!");
+    }
+    
+}
