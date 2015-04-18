a263bc9d0f8f32dbaa4a219f108d0746fdb2a370
MaciejWojtasik <maciej_wojtasik@o2.pl>
2015-04-19 00:29:08 +0200

    added power to calculator

--- src/master/Calculator.java
+++ src/master/Calculator.java
@@ -8,4 +8,14 @@ public class Calculator {
     public static float division(int a, int b){
         return a / (float)b;
     }
+    
+    public static int power(int a, int b){
+        if(b == 0)
+            return 1;
+        for(int i = 1; i < b; i++){
+            a*=a;
+        }
+        return a;
+    }
+    
 }
