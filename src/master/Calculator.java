10bb20f76e84c14466ae0632a3cae2adfb3907be
MaciejWojtasik <maciej_wojtasik@o2.pl>
2015-04-19 00:20:55 +0200

    added subtraction

--- src/master/Calculator.java
+++ src/master/Calculator.java
@@ -10,6 +10,11 @@ public class Calculator {
     }
     
     public static int addition(int a, int b){
-        return a+b;
+        return a + b;
     }
+    
+    public static int subtraction(int a, int b){
+        return a - b;
+    }
+    
 }
